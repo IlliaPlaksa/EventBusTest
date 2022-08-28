24ca789ec25a903b7ef5763abac39a728b7d17aa
Iluhap <illia.plaksa@teamdev.com>
2022-08-28 16:55:48 +0300

    Update benchmarks.cpp

--- benchmarks/benchmarks.cpp
+++ benchmarks/benchmarks.cpp
@@ -5,40 +5,30 @@
 #include <benchmark/benchmark.h>
 
 #include <memory>
-#include <thread>
-#include <mutex>
 #include <eventpp/eventqueue.h>
 #include <dexode/EventBus.hpp>
 #include "events.pb.h"
 
 const static std::string message_type = "event";
-const static size_t listeners_count = 10;
-const static size_t events_count = 10000;
-
-static size_t event_counter = 0;
-std::mutex mutex;
-std::condition_variable cv;
+const static size_t listeners_count = 20;
+const static size_t events_count = 1000;
 
 std::vector<Event> CreateEvents()
 {
     auto events = std::vector<Event>{events_count};
     
-    for (size_t i = 0; i < events.size(); ++i)
+    for (auto& event : events)
     {
         StringEvent str_event{};
-        std::string value = "value";
-        str_event.set_value(value + std::to_string(i));
+        str_event.set_value("value");
         
-        events[i].mutable_payload()->PackFrom(str_event);
+        event.mutable_payload()->PackFrom(str_event);
     }
     return events;
 }
 
 void OnEventFunc(const Event& event)
 {
-    event_counter++;
-    auto string_event = StringEvent{};
-    event.payload().UnpackTo(&string_event);
 }
 
 void PropagateEventsToEventQueue(std::shared_ptr<eventpp::EventQueue<std::string, void(const Event&)>>& queue,
@@ -55,7 +45,8 @@ void PropagateEventsToEventBus(std::shared_ptr<dexode::EventBus>& bus, const std
     }
 }
 
-std::vector<std::unique_ptr<dexode::EventBus::Listener>> RegisterEventBusListeners(std::shared_ptr<dexode::EventBus>& bus)
+std::vector<std::unique_ptr<dexode::EventBus::Listener>> 
+RegisterEventBusListeners(std::shared_ptr<dexode::EventBus>& bus)
 {
     auto listeners = std::vector<std::unique_ptr<dexode::EventBus::Listener>>{};
     for (size_t i = 0; i < listeners_count; ++i)
@@ -67,45 +58,33 @@ std::vector<std::unique_ptr<dexode::EventBus::Listener>> RegisterEventBusListene
     }
     return listeners;
 }
-void RegisterEventQueueListeners(std::shared_ptr<eventpp::EventQueue<std::string, void(const Event&)>>& queue)
+
+std::vector<eventpp::EventQueue<std::string, void(const Event&)>::Handle>
+RegisterEventQueueListeners(std::shared_ptr<eventpp::EventQueue<std::string, void(const Event&)>>& queue)
 {
+    std::vector<eventpp::EventQueue<std::string, void(const Event&)>::Handle> handles{};
     for (size_t i = 0; i < listeners_count; ++i)
     {
-        queue->appendListener(
+        auto handle = queue->appendListener(
             message_type,
             [&](const Event& event) { OnEventFunc(event); }
         );
+        
+        handles.emplace_back(handle);
     }
+    return handles;
 }
 
 static void BM_EventppEventQueue(benchmark::State& state) {
     auto events = CreateEvents();
     auto queue = std::make_shared<eventpp::EventQueue<std::string, void(const Event&)>>();
 
-    RegisterEventQueueListeners(queue);
-
-    std::atomic_bool stop = false;
-
-    std::thread worker{
-        [&queue, &stop]() {
-            while(!stop) {
-                queue->processOne();
-                cv.notify_one();
-            }
-        }
-    };
-
+    auto handles = RegisterEventQueueListeners(queue);
+    
     for (auto _ : state) {
-        event_counter = 0;
-        
         PropagateEventsToEventQueue(queue, events);
-
-        std::unique_lock lk(mutex);
-        cv.wait(lk, [&]() { return event_counter == events_count * listeners_count; });
+        queue->process();
     }
-
-    stop.store(true);
-    worker.join();
 }
 // Register the function as a benchmark
 BENCHMARK(BM_EventppEventQueue);
@@ -117,29 +96,10 @@ static void BM_DexodeEventBus(benchmark::State& state) {
 
     auto listeners = RegisterEventBusListeners(bus);
 
-    std::atomic_bool stop = false;
-
-    std::thread worker{
-        [&bus, &stop]() {
-            while (!stop) {
-                bus->process();
-
-                cv.notify_one();
-            }
-        }
-    };
-
     for (auto _ : state) {
-        event_counter = 0;
-
         PropagateEventsToEventBus(bus, events);
-
-        std::unique_lock lk(mutex);
-        cv.wait(lk, [&]() { return event_counter == events_count * listeners_count; });
+        bus->process();
     }
-
-    stop.store(true);
-    worker.join();
 }
 BENCHMARK(BM_DexodeEventBus);
 
